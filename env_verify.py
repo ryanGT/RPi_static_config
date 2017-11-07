4bcc069202d47dc92fca8a4b6345a519f1786a09
Ryan Krauss <ryanwkrauss@gmail.com>
2017-11-07 12:43:09 -0500

    added env_verify.py

---
+++ env_verify.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+
+os.system("echo $PYTHONPATH")
+
+test1 = os.path.exists("/home/pi/python_modules/serial_utils.py")
+
+if test1:
+    print("serial_utils.py is in ~/python_modules <-- bad")
+else:
+    print("serial_utils.py is not in ~/python_modules <-- good")
+
+
+test2 = os.path.exists("/home/pi/SBR_git/serial_utils.py")
+    
+if test1:
+    print("serial_utils.py is in ~/SBR_git <-- good")
+else:
+    print("serial_utils.py is not in ~/SBR_git <-- bad")
